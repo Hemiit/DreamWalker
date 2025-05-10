056f705c479fe075d3fd6aa5c9940197bf8cba1a
Hemiit <hemiit@foxmail.com>
2025-05-10 15:01:10 +0800

    Feat: manager scripts

---
+++ Assets/Scrpts/UIMgr.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMgr : MonoBehaviour
+{
+    public UI_StartGame startGame;
+
+
+    void Awake()
+    {
+        startGame.Init();
+
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
