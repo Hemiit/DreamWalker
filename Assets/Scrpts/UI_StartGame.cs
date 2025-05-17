b905744421609f5f586cea4ba382fcc79e3a715f
Hemiit <hemiit@foxmail.com>
2025-05-17 15:03:06 +0800

    Feat: DOTween and Btn_NewGame

--- Assets/Scrpts/UI_StartGame.cs
+++ Assets/Scrpts/UI_StartGame.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_StartGame : MonoBehaviour
 {
+    public Transform title;
+    public Button btn_NewGame;
+    public Button btn_Continue;
+    public Button btn_Option;
+    public Button btn_Quit;
+
 
     public void Init() 
-    { 
-        
+    {
+
+        btn_NewGame.onClick.AddListener(()=>
+        {
+            this.gameObject.SetActive(false);
+        });
+
+
+
+
     }
 
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
