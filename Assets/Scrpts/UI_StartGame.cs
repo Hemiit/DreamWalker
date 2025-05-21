70f2c3496404cc8ab46d0e139bf5b2ff9f42c3ad
Hemiit <hemiit@foxmail.com>
2025-05-21 18:01:34 +0800

    Feat: Game start title animation.

--- Assets/Scrpts/UI_StartGame.cs
+++ Assets/Scrpts/UI_StartGame.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,18 +15,21 @@ public class UI_StartGame : MonoBehaviour
 
     public void Init() 
     {
-
+        title.localPosition = new Vector3(23,728,0);
         btn_NewGame.onClick.AddListener(()=>
         {
             this.gameObject.SetActive(false);
         });
 
-
-
-
+        AnimShow();
     }
-
-
+    
+    private void AnimShow() 
+    {
+        DOTween.Sequence()
+            .AppendInterval(1f)
+            .Append(title.DOLocalMoveY(280f,1.5f));
+    }
 
     // Start is called before the first frame update
     void Start() { }
