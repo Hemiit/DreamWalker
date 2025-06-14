ca637cfc9cf704db0c3dabdb7b0764381545b645
Hemiit <hemiit@foxmail.com>
2025-06-14 14:58:49 +0800

    Feat: Can play the animation Vertically.

--- Assets/Scrpts/Player.cs
+++ Assets/Scrpts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Animator animator;
     public static Player I;
     public float speed = 5;
 
@@ -17,8 +18,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        var vertical = new Vector3(0f, Input.GetAxis("Vertical"), 0f);
+        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+        
+
+        Vector3 horizontal = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        var vertical = new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
 
         transform.position = transform.position + horizontal * Time.deltaTime * speed;
         transform.position = transform.position + vertical * Time.deltaTime * speed;
